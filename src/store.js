02a456dc2c20fa123a2df68b1ac4730f921601dd
Linlin Li <81304801+ultrahumus9000@users.noreply.github.com>
2021-09-19 10:53:26 +0100

    set up basic

--- src/store.js
+++
@@ -1,8 +0,0 @@
-import create from "zustand";
-
-let baseUrl = "http://localhost:4000";
-
-const useStore = create((set, get) => {
-  movies: [];
-  fetchAllMovies: () => {};
-});
---
+++ src/store.jsx
@@ -0,0 +1,8 @@
+import create from "zustand";
+
+let baseUrl = "http://localhost:4000";
+
+const useStore = create((set, get) => ({
+  movies: [],
+  fetchAllMovies: () => {},
+}));

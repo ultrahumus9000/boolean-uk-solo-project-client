ee705dfbf59582092c614c65815e709b86608524
Linlin Li <81304801+ultrahumus9000@users.noreply.github.com>
2021-09-20 16:28:25 +0100

    structure done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import "./App.css";
 import { Route, Switch, Redirect } from "react-router-dom";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="main">
+        <header></header>
+        <main>
+          <Switch>
+            <Route path="/" exact></Route>
+            <Route path="/" exact></Route>
+            <Route path="/" exact></Route>
+          </Switch>
+        </main>
+        <footer></footer>
+      </div>
+    </div>
+  );
 }
 
 export default App;

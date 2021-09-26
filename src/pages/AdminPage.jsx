555ba588a41e773ec5ae5322f43f51612a729d99
Linlin Li <81304801+ultrahumus9000@users.noreply.github.com>
2021-09-26 12:27:55 +0100

    admin css set up

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -14,18 +14,6 @@ import CinemaInfo from "../components/CinemaInfo";
 export default function AdminPage() {
   return (
     <div className="admin-div">
-      <AddNewEventForm />
-      <section className="recomadation-card">
-        <p>Recommadation For today </p>
-        <p>this film</p>
-        <p>this film</p>
-        <p>this film</p>
-        <p>this film</p>
-        <p>this film</p>
-      </section>
-      <section className="cinema-infomation">
-        <CinemaInfo />
-      </section>
       <section className="event">
         <p>Films for Today</p>
         <section className="event-on-day">
@@ -35,6 +23,20 @@ export default function AdminPage() {
           <p>this film</p>
           <p>this film</p>
         </section>
+      </section>
+      <section className="recomadation-card">
+        <p>Recommadation For today </p>
+        <p>this film</p>
+        <p>this film</p>
+        <p>this film</p>
+        <p>this film</p>
+        <p>this film</p>
+      </section>
+      <section className="add-event-section">
+        <AddNewEventForm />
+      </section>
+      <section className="cinema-infomation">
+        <CinemaInfo />
       </section>
     </div>
   );
